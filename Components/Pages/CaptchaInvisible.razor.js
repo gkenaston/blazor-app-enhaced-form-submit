f3998dc54a8578857c3d051d4f9b9f5ef5ce29e7
Gordon Kenaston <gkenaston@gmail.com>
2024-12-11 16:53:35 -0700

    WOW

---
+++ Components/Pages/CaptchaInvisible.razor.js
@@ -0,0 +1,19 @@
+﻿
+window.onCallback = function (response) {
+    console.log('CaptchaInvisible.onCallback');
+    var form = document.querySelector('form');
+    form.querySelector('input[name="Model.CaptchaToken"]').value = response;
+    var submitter = form.querySelector('input[type="submit"]');
+    form.requestSubmit(submitter);
+};
+
+$(function () {
+    console.log('CaptchaInvisible.ready');
+    var form = document.querySelector('form');
+    var button = form.querySelector('button[type="button"]');
+    button.addEventListener('click', function (e) {
+        console.log('button.click');
+        e.preventDefault();
+        grecaptcha.execute();
+    });
+});
--- Components/Pages/TestPage.razor.js
+++ Components/Pages/TestPage.razor.js
@@ -5,26 +5,54 @@
 //    return false;
 //});
 
-//$(document).ready(function () {
-//    console.log('TestPage.ready');
+$(function () {
+    console.log('TestPage.ready');
 
+    var form = document.querySelector('form');
+    var submitter = form.querySelector('input[type="submit"]');
+    var button = form.querySelector('button[type="button"]');
+
+    button.addEventListener('click', function (e) {
+        console.log('button.click');
+        e.preventDefault();
+        var captchaKey = form.querySelector('input[name="Model.CaptchaKey"]').value;
+        grecaptcha.execute(captchaKey, { action: 'login' }).then(function (response) {
+            form.querySelector('input[name="Model.CaptchaToken"]').value = response;
+            form.requestSubmit(submitter);
+        });
+    });
+});
+
+// good
+//(function ($) {
 //    $('#test-form').on('submit', function () {
 //        console.log('test-form.submit');
 
 //        var captchaKey = $('#test-form').find('input[name="Model.CaptchaKey"]').val();
-
-//        grecaptcha.execute(captchaKey, { action: 'login' }).then(function (response) {
-//            $('#test-form').find('input[name="Model.CaptchaToken"]').val(response);
-//        });
+//        console.log('captchaKey: ' + captchaKey);
 //    });
-//});
-
- good
+//})(jQuery);
+/*
 (function ($) {
-    $('#test-form').on('submit', function () {
-        console.log('test-form.submit');
+    // get the form
+    var form = $('form');
+    // get the submit input in the form
+    var submitter = form.find('input[type="submit"]');
+    // get the button in the form
+    var button = form.find('button[type="button"]');
 
-        var captchaKey = $('#test-form').find('input[name="Model.CaptchaKey"]').val();
-        console.log('captchaKey: ' + captchaKey);
+    button.on('click', function (e) {
+        console.log('button.click');
+        e.preventDefault();
+
+        var captchaKey = form.find('input[name="Model.CaptchaKey"]').val();
+
+        grecaptcha.execute(captchaKey, { action: 'login' }).then(function (response) {
+            form.find('input[name="Model.CaptchaToken"]').val(response);
+            let myForm = document.querySelector('form');
+            let submitButton = myForm.querySelector('input[type="submit"]');
+            myForm.requestSubmit(submitButton);
+        });
     });
 })(jQuery);
+*/
