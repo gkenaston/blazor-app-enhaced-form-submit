ae770bcf0b714fb02b9d0792506ee569f8b4bfe7
Gordon Kenaston <gkenaston@gmail.com>
2024-12-11 07:01:33 -0700

    Add project files.

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorAppEnhacedFormSubmit.Components.Pages;
+
+using Microsoft.AspNetCore.Components;
+
+public partial class Home
+{
+    [SupplyParameterFromForm] FormModel Model { get; set; } = new();
+
+    void OnSubmit()
+    {
+        Console.WriteLine($"Name: {Model.Name}");
+    }
+
+    class FormModel
+    {
+        public string? Name { get; set; }
+    }
+}
---
+++ Components/Pages/Home.razor.js
@@ -0,0 +1,11 @@
+﻿
+(function ($) {
+    $('#test-form').on('submit', function (e) {
+        e.preventDefault();
+        this.submit();
+    });
+
+    $('#page-button').on('click', function (e) {
+        $('#form-button').click();
+    });
+})(jQuery);
