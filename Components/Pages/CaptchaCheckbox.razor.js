072d0bca103768e58b9bd7c6c1c1776d16336bf7
Gordon Kenaston <gkenaston@gmail.com>
2024-12-11 17:57:21 -0700

    WOOT

---
+++ Components/Pages/CaptchaCheckbox.razor.js
@@ -0,0 +1,32 @@
+﻿
+window.verifyCallback = function (response) {
+    console.log('verifyCallback');
+
+    var form = document.querySelector('div.offcanvas.show form');
+    form.querySelector('input[name="Model.CaptchaToken"]').value = response;
+    //var submitter = form.querySelector('input[type="submit"]');
+    //form.requestSubmit(submitter);
+};
+
+var registerCaptcha, loginCaptcha;
+
+window.onloadCallback = function () {
+    console.log('onloadCallback');
+
+    registerCaptcha = grecaptcha.render('register-captcha', {
+        'sitekey': '6LdbGRcaAAAAANiUivxhzoifj-wK83WnS7jJ-Xq9',
+        'callback': 'verifyCallback'
+    });
+
+    loginCaptcha = grecaptcha.render('login-captcha', {
+        'sitekey': '6LdbGRcaAAAAANiUivxhzoifj-wK83WnS7jJ-Xq9',
+        'callback': 'verifyCallback'
+    });
+};
+
+$(function () {
+    var form = document.querySelector('div.offcanvas.show form');
+    console.log(form);
+
+    //form.querySelector('input[name="Model.CaptchaToken"]').value = 'response';
+});
